396a17ef4a21bef6e7c55102dc6909062726f841
Bostjan Vizintin <bostjan.vizintin@gmail.com>
2017-02-22 16:08:41 +0100

    added notifications

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -11,8 +11,8 @@ use App\Http\Requests;
 class NotificationController extends Controller
 {
     public function index(Request $request) {
-      $sensorBoxes = App\SensorBox::where('idUser', \Auth::user()->id)->get();
-      $sensorBoxes->load('sensors');
-      return view('notifications')->with('sensorBoxes', $sensorBoxes);
+      $notifications  = App\Notification::where('idUser', \Auth::user()->id)->where('active','>', 0)->get();
+
+      return view('notifications')->with('notifications', $notifications);
     }
 }
--- resources/views/notifications.blade.php
+++ resources/views/notifications.blade.php
@@ -13,7 +13,42 @@
             @include('includes.mainButtons')
           </div>
           <div class="panel-body">
-              asdf
+            <div class="">
+
+            </div>
+            <div class="notifications-table">
+              <table class="table table-striped">
+                <thead>
+                  <tr>
+                    <th>Sensor</th>
+                    <th>Name</th>
+                    <th>Value</th>
+                    <th>Status</th>
+                    <th>Action</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  @foreach ($notifications as $notification)
+                    @if($notification['active'] == 1)
+                      <tr class="success">
+                    @else
+                      <tr class="danger">
+                    @endif
+                      <td>{{ $notification['idSensor'] }}</td>
+                      <td>{{ $notification['name'] }}</td>
+                      <td>{{ $notification['value'] }}</td>
+                      @if($notification['active'] == 1)
+                        <td>Active</td>
+                        <td><button type="button" name="button" class="btn btn-info btn-notifications">Cancel</button></td>
+                      @else
+                        <td>Triggered</td>
+                        <td><button type="button" name="button" class="btn btn-info btn-notifications">Hide</button></td>
+                      @endif
+                    </tr>
+                  @endforeach
+                </tbody>
+              </table>
+            </div>
               @if (isset($errors) && count($errors) > 0)
                   <div class="alert alert-danger">
                       <ul>
